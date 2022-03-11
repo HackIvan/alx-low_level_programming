a92888038f8a3865766a2b2d9462e7f98178f9c7
Ivan <ibrahimivank@hotmail.com>
2022-03-11 13:25:36 -0800

    correct code added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-/**       
- * Main entry point of this program
- * Returns (0)
+/**
+ * main - Entry point
  *
+ * Return: Always 0 (Success Message)
  */
 
 int main(void)
 {
-        char mychar;
-        int myint;
-        long mylong;
-        long long mylongx2;
-        float myfloat;
+	char mycharacter;
+	int myinteger;
+	long mylong;
+	long long mylonglong;
+	float myfloat;
 
+	printf("Size of a char: %lu byte(s)\n", sizeof(mycharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(myint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(mylong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(mylonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(myfloat));
 
-
-        printf("Size of my int is: %lu \n", sizeof(myint));
-        printf("size of my char is: %lu\n", sizeof(mychar));
-        printf("Size of my long long is: %lu\n", sizeof(mylongx2));
-        printf("Size of my long is: %lu\n", sizeof(mylong));
-        printf("Size of my float is: %lu\n", sizeof (myfloat));
-
-        return (0);
+	return (0);
 }
-
