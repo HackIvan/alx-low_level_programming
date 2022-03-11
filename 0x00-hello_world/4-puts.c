602522671dfdfaa15834b64571258cd3d94cb65d
Ivan <ibrahimivank@hotmail.com>
2022-03-11 13:54:59 -0800

    actual bett style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * This Prints exactly as it is.
+ * main - Entry point
  *
- * without andy error
+ * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
