88fca6290ddf3a26bfc666e6f296ea44bc0685f5
Ivan <ibrahimivank@hotmail.com>
2022-03-11 13:37:51 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * This Prints exactly as it is.
+ *
+ * without andy error
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
